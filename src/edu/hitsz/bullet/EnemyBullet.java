49eb6e24733ffe8683972006e9ad996577fb5298
ScarecrowL1 <ScarecrowL1>
2022-03-24 18:52:41 +0800

    Changes

--- src/edu/hitsz/bullet/EnemyBullet.java
+++ src/edu/hitsz/bullet/EnemyBullet.java
@@ -3,12 +3,9 @@ package edu.hitsz.bullet;
 /**
  * @Author hitsz
  */
-public class EnemyAbstractBullet extends AbstractBullet {
+public class EnemyBullet extends AbstractBullet {
 
-    public static final int WIDTH=6;
-    public static final int HEIGHT=18;
-
-    public EnemyAbstractBullet(int locationX, int locationY, int speedX, int speedY, int power) {
+    public EnemyBullet(int locationX, int locationY, int speedX, int speedY, int power) {
         super(locationX, locationY, speedX, speedY, power);
     }
 
---
+++ src/edu/hitsz/prop/AbstracyProp.java
@@ -0,0 +1,6 @@
+package edu.hitsz.prop;
+
+import edu.hitsz.basic.FlyingObject;
+
+public abstract class AbstracyProp extends FlyingObject {
+}
